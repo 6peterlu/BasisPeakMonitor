efa71be7f2db2a7f3d7b5cf2e2a3546f6f15b5b7
Peter Lu <peterlu6@stanford.edu>
2016-03-21 22:00:04 -0700

    Added basis.txt to raw file, included file reading code!(:

--- app/src/main/java/com/example/peter/basispeakmonitor/MainActivity.java
+++ app/src/main/java/com/example/peter/basispeakmonitor/MainActivity.java
@@ -5,21 +5,8 @@ import android.os.Bundle;
 import android.util.Log;
 import android.widget.TextView;
 
-import com.google.gson.JsonObject;
-import com.koushikdutta.async.AsyncServer;
-import com.koushikdutta.async.future.FutureCallback;
-import com.koushikdutta.async.http.AsyncHttpClient;
-import com.koushikdutta.ion.Ion;
+import java.util.Scanner;
 
-import org.json.JSONException;
-import org.json.JSONObject;
-
-import java.io.IOException;
-import java.net.HttpURLConnection;
-import java.net.MalformedURLException;
-import java.net.URL;
-import java.security.KeyPair;
-import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -27,40 +14,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-
-        //Login attempt here: doesn't work
-        JsonObject json = new JsonObject();
-        json.addProperty("username", "peterlu6@stanford.edu");
-        json.addProperty("password", "basispeaktest");
-
-        Ion.with(this)
-                .load("https://app.mybasis.com/login")
-                .setJsonObjectBody(json)
-                .asString()
-                .setCallback(new FutureCallback<String>() {
-                    @Override
-                    public void onCompleted(Exception e, String result) {
-                        System.out.println("OUTPUT HERE: " + result);
-                    }
-                });
-
-        //Getting data attempt here: does work
-        Ion.with(this)
-                .load("https://app.mybasis.com/api/v1/metricsday/me?day=2016-03-20&padding=10800&heartrate=true&steps=true&calories=true&gsr=true&skin_temp=true&bodystates=true")
-                .asString()
-                .setCallback(new FutureCallback<String>() {
-                    public void onCompleted(Exception e,
-                                            String result) {
-                        processResult(result);
-                    }
-                });
-
-
-
-    }
-    private void processResult(String data){
         TextView test = (TextView)findViewById(R.id.raw_data);
+        Scanner s = new Scanner(getResources().openRawResource(R.raw.basis));
+        String data="";
+        try {
+            while (s.hasNext()) {
+                data = data+s.next();
+            }
+        } finally {
+            s.close();
+        }
         test.setText(data);
     }
 }
