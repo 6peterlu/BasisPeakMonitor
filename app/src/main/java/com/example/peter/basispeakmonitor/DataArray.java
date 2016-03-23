4522b675c55a34d201013e06cea4c886916f3f7a
Peter Lu <peterlu6@stanford.edu>
2016-03-22 17:31:25 -0700

    DataArray class added

---
+++ app/src/main/java/com/example/peter/basispeakmonitor/DataArray.java
@@ -0,0 +1,17 @@
+package com.example.peter.basispeakmonitor;
+
+import java.util.ArrayList;
+
+/**
+ * Created by Peter on 3/22/2016.
+ */
+public class DataArray {
+    private ArrayList<Integer> rawData;
+    private ArrayList<Integer> truncatedData;
+
+
+    public DataArray(ArrayList<String> input){
+        
+    }
+
+}
--- app/src/main/java/com/example/peter/basispeakmonitor/MainActivity.java
+++ app/src/main/java/com/example/peter/basispeakmonitor/MainActivity.java
@@ -24,6 +24,7 @@ public class MainActivity extends AppCompatActivity {
         } finally {
             s.close();
         }
+
         test.setText(data);
     }
 }
