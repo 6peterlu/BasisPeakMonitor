a9578bc5886f6dfe75422eb95933ac8b04734b92
Peter Lu <peterlu6@stanford.edu>
2016-03-22 18:43:24 -0700

    fixed parsing for gsr array: MainActivity

--- app/src/main/java/com/example/peter/basispeakmonitor/MainActivity.java
+++ app/src/main/java/com/example/peter/basispeakmonitor/MainActivity.java
@@ -30,14 +30,20 @@ public class MainActivity extends AppCompatActivity {
         String[] steps = getData("steps", data);
         String[] gsr = getData("gsr", data);
         String[] skin_temp = getData("skin_temp", data);
-        
+
+        String gsrString = "";
+        for(String sample: gsr){
+            gsrString = gsrString+sample;
+        }
+        test.setText(gsrString);
+
     }
 
     //Helper methods
 
     /**
      * Parses string into a string array.
-     * @return
+     * @return String array containing parsed data
      */
     private static String[] getData(String metric, String raw){
         int index = raw.indexOf(metric);
@@ -49,7 +55,29 @@ public class MainActivity extends AppCompatActivity {
             int endBracket = raw.indexOf("]", index);
             String numbers = raw.substring(startBracket+1,endBracket);
             String[] parsed = numbers.split(",");
+            if(metric.equals("gsr")){
+                for(int i = 0; i<parsed.length; i++){
+                    if(!parsed[i].equals("null")) {
+                        parsed[i] = convertFromScientific(parsed[i]);
+                    }
+                }
+            }
             return parsed;
         }
+    }
+
+    //format: "5.07e-05" -> "0.0000507"
+    private static String convertFromScientific(String input){
+        String output = "0.";
+        int eLoc = input.indexOf('e');
+        int powerTen = Integer.parseInt(input.substring(eLoc+1));
+        for(int i = 1; i<-powerTen; i++){
+            output = output+"0";
+        }
+        for(int i = 0; i<eLoc; i++){
+            if(input.charAt(i)=='.') continue;
+            output = output+input.charAt(i);
+        }
+        return output;
     }
 }
